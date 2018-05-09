c7abcf64f24f9f3778c15fa6d748e97d1c8f94f8
yannickholzenkamp <yannick@holzenkamp.me>
2018-05-09 20:06:59 +0200

    refactor

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ use \Psr\Http\Message\ResponseInterface as Response;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 
 require 'vendor/autoload.php';
-require 'requests.php';
 spl_autoload_register('autoloader');
 
 $app = new \Slim\App;
